3ebc6b95559f07fb698cf4d3ccae2bd034af42d0
CortezStephanie <cortezstephanie2389@yahoo.com>
2022-08-10 23:25:22 -0500

    character arrays created and function to hold code

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const specialCharacters = ["!","@","#","$","%","^","&","*","(",")","-","+","=","_"];
+const numberCharacters = ["0","1","2","3","4","5","6","7","8","9"]
+
+const generateBtn = document.getElementById("generateBtn");
+const genPassword = document.getElementById("genPassword");
+
+function generatedRandomPassword () {
+    console.log("inside the function");
+    let password = genPassword;
+    
+}
+
+
+generateBtn.addEventListener("click", generatedRandomPassword )
+
